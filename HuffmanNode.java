d2c9d8e9df7be6d3d7aafb83885e99ce306ed597
MuratCiftci <44578841+MuratCiftci@users.noreply.github.com>
2018-12-06 12:04:20 +0300

    Add files via upload

---
+++ HuffmanNode.java
@@ -0,0 +1,22 @@
+/**
+ * Huffman düğüm sınıfı. Bu sınıfın içeriğini değiştirmemeniz gerekmektedir. Değişiklik yaparsanız test ortamında kodunuz beklendiği gibi çalışmayacaktır.
+ */
+public class HuffmanNode implements Comparable{
+    public Character value;
+    public int frequency;
+    public HuffmanNode left;
+    public HuffmanNode right;
+
+    public HuffmanNode(Character value, int frequency, HuffmanNode left, HuffmanNode right) {
+        this.value = value;
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        HuffmanNode oo=(HuffmanNode)o;
+        return this.frequency-oo.frequency;
+    }
+}
